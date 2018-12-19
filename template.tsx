024bf5c44677359c0dddbd672aaf32e793e05e11
houjunjie <376969465@qq.com>
2018-12-19 16:27:50 +0800

    增加connect-component组件

--- template.tsx
+++ template.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { IBaseProps } from '@/interfaces/common';
+import { connect } from 'dva';
 
 interface Props extends IBaseProps {
 }
@@ -7,6 +8,9 @@ interface Props extends IBaseProps {
 interface State {
 }
 
+@connect(({ loading }:Props) => ({
+  loading
+}))
 class {{template}} extends React.Component<Props, State> {
   state:State = {
   }
